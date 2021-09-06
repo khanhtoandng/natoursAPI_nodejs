17a2291fbd5c1d2d922a030e5e6fd8cd31a9da44
khanhtoandng <khanhtoan.dng@gmail.com>
2021-09-06 16:25:46 +0700

    handling crud api

---
+++ app.js
@@ -0,0 +1,21 @@
+const fs  = require('fs');
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// MIDDLEWARES
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+// ROUTES
+app.use('/api/tours', tourRouter);
+app.use('/api/users', userRouter);
+    
+module.exports = app;
