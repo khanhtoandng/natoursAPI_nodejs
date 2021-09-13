f084ecd3cb55e7e9dd4b45032ae26e502a107273
khanhtoandng <khanhtoan.dng@gmail.com>
2021-09-13 17:39:38 +0700

    Add forgetPassword feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -98,3 +98,17 @@ exports.restrictTo = (...roles) => {
         next();
     };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+    const user = await User.findOne({ email: req.user.email });
+    if(!user) {
+        return next(
+            new AppError('There is no user with email address!', 404)
+        )
+    };
+
+    const resetToken = user.createPasswordResetToken();
+    await user.save({ validateBeforeSave: false });
+
+
+});
